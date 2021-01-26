831acf855eb9b49eb35ec9f8cf0dec550bd2ecfc
Haris Mexis <theoharismexis@hotmail.com>
2021-01-26 13:58:55 +0000

    added ItemDetailActivity

--- app/src/main/java/com/example/jsonfeed/detail/ItemDetailActivity.kt
+++ app/src/main/java/com/example/jsonfeed/detail/ItemDetailActivity.kt
@@ -1,11 +1,12 @@
-package com.example.jsonfeed
+package com.example.jsonfeed.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.jsonfeed.R
 
-class MainActivity : AppCompatActivity() {
+class ItemDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_item_detail)
     }
 }
---
+++ app/src/main/java/com/example/jsonfeed/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jsonfeed.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jsonfeed.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
