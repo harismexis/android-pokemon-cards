123364f9ae7a76b8a733ce822754fffb023034dd
Haris Mexis <theoharismexis@hotmail.com>
2021-01-26 19:11:37 +0000

    removed unneeded dependencies

---
+++ app/src/main/java/com/example/jsonfeed/di/ActivityBindingsModule.kt
@@ -0,0 +1,17 @@
+package com.example.jsonfeed.di
+
+import com.example.jsonfeed.detail.ui.ItemDetailActivity
+import com.example.jsonfeed.home.ui.HomeActivity
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ActivityBindingsModule {
+
+    @ContributesAndroidInjector
+    abstract fun homeActivity(): HomeActivity
+
+    @ContributesAndroidInjector
+    abstract fun itemDetailActivity(): ItemDetailActivity
+
+}
