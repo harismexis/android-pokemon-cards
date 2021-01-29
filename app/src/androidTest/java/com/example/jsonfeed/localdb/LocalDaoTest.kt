1d90d130117756a5490e6fd1f5eadeda8074e2c7
Haris Mexis <theoharismexis@hotmail.com>
2021-01-29 17:29:52 +0000

    added Android Tests for MainApplication and Room (basic setup)

---
+++ app/src/androidTest/java/com/example/jsonfeed/localdb/LocalDaoTest.kt
@@ -0,0 +1,47 @@
+package com.example.jsonfeed.localdb
+
+
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class LocalDaoTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var dao: LocalDao
+    private lateinit var database: LocalDatabase
+
+    @Before
+    fun createDb() {
+        val context: Context = ApplicationProvider.getApplicationContext()
+        database = Room.inMemoryDatabaseBuilder(context, LocalDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        dao = database.localDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        database.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun savedToken_isSameWithRetrievedToken() = runBlocking {
+
+    }
+
+}
