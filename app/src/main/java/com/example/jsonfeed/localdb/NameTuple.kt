2c79a4718b313acb5b869ac820f8df9e54400b19
Haris Mexis <theoharismexis@hotmail.com>
2021-01-30 01:22:31 +0000

    removed uneeded class

--- app/src/main/java/com/example/jsonfeed/localdb/NameTuple.kt
+++
@@ -1,8 +0,0 @@
-package com.example.jsonfeed.localdb
-
-import androidx.room.ColumnInfo
-
-data class NameTuple(
-    @ColumnInfo(name = "feed_item_name") val itemName: String?,
-    @ColumnInfo(name = "feed_item_metadata") val itemMeta: String?
-)
