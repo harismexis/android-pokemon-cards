032544fd01ab75f87fed0cf93ec650c188713830
Haris Mexis <theoharismexis@hotmail.com>
2021-01-31 14:22:18 +0000

    working on tests

---
+++ app/src/test/java/com/example/jsonfeed/testutils/MockFeed.kt
@@ -0,0 +1,5 @@
+package com.example.jsonfeed.testutils
+
+fun getMockFeed(): String {
+    return ""
+}
---
+++ app/src/test/java/com/example/jsonfeed/testutils/MockFeedProvider.kt
@@ -0,0 +1,17 @@
+package com.example.jsonfeed.testutils
+
+import com.example.jsonfeed.datamodel.Feed
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+
+private fun convertToCreditResponse(jsonString: String?): Feed {
+    val gson = GsonBuilder().setLenient().create()
+    val json: JsonObject = gson.fromJson(jsonString, JsonObject::class.java)
+    return Gson().fromJson(json, Feed::class.java)
+}
+
+fun provideMockFeed(): Feed {
+    return convertToCreditResponse(getMockFeed())
+}
