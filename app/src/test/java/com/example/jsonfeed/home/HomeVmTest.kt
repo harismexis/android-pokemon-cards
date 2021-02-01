9873c09ba690c4de76fd643ea4da29045fe28801
Haris Mexis <theoharismexis@hotmail.com>
2021-02-01 16:35:47 +0000

    improved unit tests

--- app/src/test/java/com/example/jsonfeed/home/HomeVmTest.kt
+++ app/src/test/java/com/example/jsonfeed/home/HomeVmTest.kt
@@ -4,14 +4,11 @@ import com.example.jsonfeed.extensions.toLocalItems
 import com.example.jsonfeed.extensions.toUiModels
 import com.example.jsonfeed.mockprovider.provideMockFeedValid
 import com.example.jsonfeed.mockprovider.provideMockLocalItemsValid
-import com.nhaarman.mockitokotlin2.verify
-import kotlinx.coroutines.runBlocking
+
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import org.mockito.Mockito
-import org.mockito.Mockito.`when`
 
 @RunWith(JUnit4::class)
 class HomeVmTest : HomeVmTestSetup() {
@@ -27,20 +24,16 @@ class HomeVmTest : HomeVmTestSetup() {
         val mockFeedValid = provideMockFeedValid()
         val expectedUiModels = mockFeedValid.toUiModels()
         val expectedLocalItems = expectedUiModels.toLocalItems()
-        `when`(mockConnectivity.isOnline()).thenReturn(true)
-        runBlocking {
-            `when`(mockFeedRepo.getJsonFeed()).thenReturn(mockFeedValid)
-        }
+        mockInternetActive(true)
+        mockFeedNetworkCall(mockFeedValid)
 
         // when
         homeVm.bind()
 
         // then
-        runBlocking {
-            verify(mockFeedRepo, Mockito.times(1)).getJsonFeed()
-            verify(observer).onChanged(expectedUiModels)
-            verify(mockLocalRepo, Mockito.times(1)).insertItems(expectedLocalItems)
-        }
+        verifyRemoteDataRetrieved()
+        verifyLiveDataChanged(expectedUiModels)
+        verifyDataStoredLocally(expectedLocalItems)
     }
 
     @Test
@@ -48,20 +41,33 @@ class HomeVmTest : HomeVmTestSetup() {
         // given
         val mockLocalItems = provideMockLocalItemsValid()
         val expectedUiModels = mockLocalItems.toUiModels()
-        `when`(mockConnectivity.isOnline()).thenReturn(false)
-        runBlocking {
-            `when`(mockLocalRepo.getAllItems()).thenReturn(mockLocalItems)
-        }
+        mockInternetActive(false)
+        mockLocalItemsCall(mockLocalItems)
 
         // when
         homeVm.bind()
 
         // then
-        runBlocking {
-            verify(mockFeedRepo, Mockito.times(0)).getJsonFeed()
-            verify(mockLocalRepo, Mockito.times(1)).getAllItems()
-            verify(observer).onChanged(expectedUiModels)
-        }
+        verifyLocalItemsRetrieved()
+        verifyLiveDataChanged(expectedUiModels)
+    }
+
+    @Test
+    fun onRefresh_dataRefreshedIfInternetActive() {
+        // given
+        val mockFeedValid = provideMockFeedValid()
+        val expectedUiModels = mockFeedValid.toUiModels()
+        val expectedLocalItems = expectedUiModels.toLocalItems()
+        mockInternetActive(true)
+        mockFeedNetworkCall(mockFeedValid)
+
+        // when
+        homeVm.refresh {}
+
+        // then
+        verifyRemoteDataRetrieved()
+        verifyLiveDataChanged(expectedUiModels)
+        verifyDataStoredLocally(expectedLocalItems)
     }
 
 }
--- app/src/test/java/com/example/jsonfeed/home/HomeVmTestSetup.kt
+++ app/src/test/java/com/example/jsonfeed/home/HomeVmTestSetup.kt
@@ -1,13 +1,18 @@
 package com.example.jsonfeed.home
 
 import androidx.lifecycle.Observer
+import com.example.jsonfeed.datamodel.Feed
 import com.example.jsonfeed.repository.FeedRepository
 import com.example.jsonfeed.home.viewmodel.HomeVm
+import com.example.jsonfeed.localdb.LocalItem
 import com.example.jsonfeed.shared.ViewModelBaseTestSetup
 import com.example.jsonfeed.uimodel.UiModel
 import com.example.jsonfeed.util.network.ConnectivityMonitor
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
 
 import org.mockito.Mock
+import org.mockito.Mockito
 
 abstract class HomeVmTestSetup : ViewModelBaseTestSetup() {
 
@@ -31,16 +36,45 @@ abstract class HomeVmTestSetup : ViewModelBaseTestSetup() {
 
     }
 
-    protected fun setupMockFeedRepo() {
-
+    protected fun mockInternetActive(active: Boolean) {
+        Mockito.`when`(mockConnectivity.isOnline()).thenReturn(active)
     }
 
-    private fun setupMockConnectivity() {
-
+    protected fun mockFeedNetworkCall(feed: Feed) {
+        runBlocking {
+            Mockito.`when`(mockFeedRepo.getJsonFeed()).thenReturn(feed)
+        }
     }
 
-    private fun setupMockResponse() {
+    protected fun mockLocalItemsCall(localItems: List<LocalItem>) {
+        runBlocking {
+            Mockito.`when`(mockLocalRepo.getAllItems()).thenReturn(localItems)
+        }
+    }
 
+    protected fun verifyLocalItemsRetrieved() {
+        runBlocking {
+            verify(mockFeedRepo, Mockito.never()).getJsonFeed()
+            verify(mockLocalRepo, Mockito.times(1)).getAllItems()
+            verify(mockLocalRepo, Mockito.never()).insertItems(com.nhaarman.mockitokotlin2.any())
+        }
+    }
+
+    protected fun verifyRemoteDataRetrieved() {
+        runBlocking {
+            verify(mockFeedRepo, Mockito.times(1)).getJsonFeed()
+            verify(mockLocalRepo, Mockito.never()).getAllItems()
+        }
+    }
+
+    protected fun verifyLiveDataChanged(uiModels: List<UiModel>) {
+        verify(observer).onChanged(uiModels)
+    }
+
+    protected fun verifyDataStoredLocally(expectedLocalItems: List<LocalItem>) {
+        runBlocking {
+            verify(mockLocalRepo, Mockito.times(1)).insertItems(expectedLocalItems)
+        }
     }
 
 }
