e956b8f6b3902a5a135494aae40332f42232979a
Haris Mexis <theoharismexis@hotmail.com>
2021-03-31 23:16:39 +0100

    updates

--- app/src/main/java/com/harismexis/pokemon/uimodel/UiModel.kt
+++ app/src/main/java/com/harismexis/pokemon/uimodel/UiModel.kt
@@ -1,15 +1,15 @@
 package com.harismexis.pokemon.uimodel
 
 data class UiModel(
-    var id: String,
-    var name: String?,
-    var imageUrl: String?,
-    var imageUrlHiRes: String?,
-    var supertype: String?,
-    var subtype: String?,
-    var artist: String?,
-    var rarity: String?,
-    var series: String?,
-    var set: String?,
-    var setCode: String?
+    val id: String,
+    val name: String?,
+    val imageUrl: String?,
+    val imageUrlHiRes: String?,
+    val supertype: String?,
+    val subtype: String?,
+    val artist: String?,
+    val rarity: String?,
+    val series: String?,
+    val set: String?,
+    val setCode: String?
 )
