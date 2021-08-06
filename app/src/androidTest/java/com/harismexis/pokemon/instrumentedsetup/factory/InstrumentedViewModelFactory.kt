8a709d5c6636f1ff35587ca432d73b405c4d3438
Haris Mexis <theoharismexis@hotmail.com>
2021-08-06 18:24:04 +0100

    updates

--- app/src/androidTest/java/com/harismexis/pokemon/instrumentedsetup/factory/InstrumentedViewModelFactory.kt
+++ app/src/androidTest/java/com/harismexis/pokemon/instrumentedsetup/factory/InstrumentedViewModelFactory.kt
@@ -5,13 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.harismexis.pokemon.presentation.detail.viewmodel.ItemDetailVm
 import com.harismexis.pokemon.presentation.home.viewmodel.HomeVm
 import dagger.Binds
-import dagger.MapKey
 import dagger.Module
 import io.mockk.mockk
 import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
-import kotlin.reflect.KClass
 
 val mockHomeVm: HomeVm = mockk(relaxed = true)
 val mockItemDetailVm: ItemDetailVm = mockk(relaxed = true)
@@ -28,15 +26,6 @@ class InstrumentedViewModelFactory @Inject constructor() : ViewModelProvider.Fac
     override fun <T : ViewModel?> create(modelClass: Class<T>) =
         getMockVmMap()[modelClass]?.get() as T
 }
-
-@Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.PROPERTY_SETTER
-)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@MapKey
-internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
 
 @Module
 abstract class InstrumentedViewModelModule {
